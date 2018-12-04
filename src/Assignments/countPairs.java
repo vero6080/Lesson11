4fbdb9103b922d2f01f56b0f2d6a58266c7ae0b0
veron <veron@192.168.1.16>
2018-12-04 12:58:05 -0500

    Modified countPairs

--- src/Assignments/countPairs.java
+++ src/Assignments/countPairs.java
@@ -13,10 +13,18 @@ public class countPairs {
         expected = ex;
         id = name;
     }
+    
+    public void test() {
+        System.out.println("Expected\t\tRun\tResult");
+        System.out.println("====================================");
+    }
 
     public static void main(String[] args) {
         ArrayList<Integer> in = new ArrayList();
         ArrayList<Integer> out = new ArrayList();
+        
+        countPairs prob = new countPairs(in, out, "array11");
+        prob.test();
     }
     
 }
